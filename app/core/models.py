aa38a2a358978b363a27171bf9094fbd9ca9e591
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-01 23:28:47 +0000

    Update models.py

--- app/core/models.py
+++ app/core/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import(
-    AbstractBaseUser, 
-    BaseUserManager, 
-    PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
     )
 
 
