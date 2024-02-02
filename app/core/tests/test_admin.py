8db1e007a8051c96560bcc6b11c18efa35b62a8f
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-02-02 22:50:25 +0000

    Create test_admin.py

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,25 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class TestsAdmin(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@example.com',
+            password = 'testPass123',
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'Userpass123',
+            name = 'Test User',
+        )
+
+    def test_users_list(self):
+        url = reverse('admin:core_user_changelist')
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
