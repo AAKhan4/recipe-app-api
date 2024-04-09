b743d8d00852ad7c27f4274e4f2969bd38348d37
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-04-09 21:17:04 +0200

    Update views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -26,31 +26,26 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.UpdateModelMixin,
+class BassRecipeAttrViewSet(
+                 mixins.UpdateModelMixin,
                  mixins.DestroyModelMixin,
                  mixins.ListModelMixin,
                  viewsets.GenericViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class TagViewSet(BassRecipeAttrViewSet):
     '''Manage Tags in DB'''
 
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
 
-class IngredientViewSet(
-    mixins.ListModelMixin,
-    viewsets.GenericViewSet,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin):
+class IngredientViewSet(BassRecipeAttrViewSet):
     '''Manage Ingredients in DB'''
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return self.queryset.filter(user=self.request.user).order_by('-name')
