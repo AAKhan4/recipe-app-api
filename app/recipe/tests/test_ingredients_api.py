a22960d1ac811f9e1b8173b3d9ac2977afa4bcf3
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-04-09 19:59:21 +0200

    Create test_ingredients_api.py

---
+++ app/recipe/tests/test_ingredients_api.py
@@ -0,0 +1,60 @@
+'''Test Ingredients API'''
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Ingredient
+
+from recipe.serializers import IngredientSerializer
+
+
+INGREDIENTS_URL = reverse('recipe:ingredient-list')
+
+
+def create_user(email='test@example.com', password='testpass123'):
+    return get_user_model().objects.create_user(email, password)
+
+
+class PublicIngredientsAPITests(TestCase):
+    '''Test unauth ingredients API reqs'''
+    def setUp(self):
+        self.client = APIClient()
+    
+    def test_auth_required(self):
+        response = self.client.get(INGREDIENTS_URL)
+        
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateIngredientsAPITests(TestCase):
+    '''Test auth ingredients API reqs'''
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_ingredients_list(self):
+        Ingredient.objects.create(user=self.user, name='Kale')
+        Ingredient.objects.create(user=self.user, name='Apple')
+
+        response = self.client.get(INGREDIENTS_URL)
+
+        ingredients = Ingredient.objects.all().order_by('-name')
+        serializer = IngredientSerializer(ingredients, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_ingredients_limited_to_user(self):
+        user2 = create_user()
+        Ingredient.objects.create(user=user2, name='Vinegar')
+        ingredient = Ingredient.objects.create(user=self.user, name='Tumeric')
+
+        response = self.client.get(INGREDIENTS_URL)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['name'], ingredient.name)
+        self.assertEqual(response.data[0]['id'], ingredient.id)
