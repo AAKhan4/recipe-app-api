85d655327618986534fa8b4571936a0eb9da9243
AAKhan4 <a.a.khan8@lancaster.ac.uk>
2024-04-10 15:21:25 +0200

    Update test_models.py

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,4 @@
+from unittest.mock import patch
 from django.test import TestCase
 from decimal import Decimal
 from django.contrib.auth import get_user_model
@@ -75,3 +76,11 @@ class ModelsTest(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None, 'example.jpg')
+
+        self.assertEqual(file_path, f'uploads/recipe/{uuid}.jpg')
